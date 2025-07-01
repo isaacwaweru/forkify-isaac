bfa22449428dc3ed0008333cb3f4065292e9fba0
Isaac Waweru <waweruisaac25@gmail.com>
2025-07-01 10:08:01 +0300

    down in

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import View from './View.js';
-import icons from '../../img/icons.svg';
+import icons from 'url:../../img/icons.svg';
 import fracty from 'fracty';
 
 class RecipeView extends View {
