4efd4bcb2105a4b04d9b3b766090ccd88c16fffa
Isaac Waweru <waweruisaac25@gmail.com>
2025-07-01 09:30:25 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a5fb41c9-5aa2-476a-8be0-ab0aa799f1cd';
+export const MODAL_CLOSE_SEC = 2.5;
